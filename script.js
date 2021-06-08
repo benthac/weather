8f78201d789a3441ced9affcdf4d93dc031d491d
ben thacker <benthac@gmail.com>
2021-06-08 17:11:23 -0400

    fixed javascript issue

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let weatherApiOne = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=54654b2970f4ff24c637b2ecba8f2f9f"
+
 let weatherSearch = function() {
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=54654b2970f4ff24c637b2ecba8f2f9f")
+    fetch(weatherApiOne)
     .then(function(response) {
     response.json().then(function(data) {
         console.log(data);
     });
-    )};
+})};
