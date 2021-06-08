c80633e90bba0e9274368995776fea44053f31a4
ben thacker <benthac@gmail.com>
2021-06-07 22:03:13 -0400

    apikey

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-//console.log("hi");
+let weatherSearch = function() {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={ 54654b2970f4ff24c637b2ecba8f2f9f}");
+
+};
